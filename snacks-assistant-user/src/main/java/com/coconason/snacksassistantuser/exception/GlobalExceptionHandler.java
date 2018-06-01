ce2827e0b872192eaee4a2c9778900cc1963b446
wgy8283335 <wgy8283335@126.com>
2018-06-01 14:49:40 +0800

    Initialize

---
+++ snacks-assistant-user/src/main/java/com/coconason/snacksassistantuser/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.coconason.snacksassistantuser.exception;
+
+import com.coconason.snacksassistantcommon.constant.ErrorCode;
+import com.coconason.snacksassistantcommon.model.ArgumentInvalidResult;
+import com.coconason.snacksassistantcommon.model.SnacksResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value= MethodArgumentNotValidException.class)
+    public SnacksResult MethodArgumentNotValidHandler(HttpServletRequest request, MethodArgumentNotValidException exception) throws Exception{
+        //按需重新封装需要返回的错误信息
+        List<ArgumentInvalidResult> invalidArguments = new ArrayList<>();
+        //解析原错误信息，封装后返回，此处返回非法的字段名称，原始值，错误信息
+        for (FieldError error : exception.getBindingResult().getFieldErrors()) {
+            ArgumentInvalidResult invalidArgument = new ArgumentInvalidResult();
+            invalidArgument.setDefaultMessage(error.getDefaultMessage());
+            invalidArgument.setField(error.getField());
+            invalidArgument.setRejectedValue(error.getRejectedValue());
+            invalidArguments.add(invalidArgument);
+        }
+        return new SnacksResult<List<ArgumentInvalidResult>>().build(ErrorCode.PARAM_ERROR.value(),ErrorCode.PARAM_ERROR.msg(),invalidArguments);
+    }
+    @ExceptionHandler(value= Exception.class)
+    public SnacksResult ExceptionHandler(HttpServletRequest request, Exception exception) throws Exception{
+        return new SnacksResult().build(ErrorCode.SYS_ERROR.value(),ErrorCode.SYS_ERROR.msg());
+    }
+}
